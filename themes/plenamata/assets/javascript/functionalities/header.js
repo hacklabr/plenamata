658eb24e263df6568c3a3885c29846d7c41e66f2
Isaque Melo <isaquegbmelo2@gmail.com>
2021-06-17 14:13:42 -0300

    Mobile header fixes

--- themes/plenamata/assets/javascript/functionalities/header.js
+++ themes/plenamata/assets/javascript/functionalities/header.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector('button.toggle-menu').addEventListener('click', function() {
         this.classList.toggle('active');
+        this.parentNode.classList.toggle('active');
+        this.closest('.main-header').classList.toggle('active');
     })
 })
