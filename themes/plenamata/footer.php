a6a7c68e5c27e22fc71537d0a0a050d1deabcb82
Isaque Melo <isaquegbmelo2@gmail.com>
2021-06-17 14:54:50 -0300

    Add footer sidebars / Add size function to convert px to rem / Add sidebar files into exports

--- themes/plenamata/footer.php
+++ themes/plenamata/footer.php
@@ -2,7 +2,18 @@
 <?php wp_reset_postdata() ?>
 
 <footer class="main-footer">
-   
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 logos-area">
+                    <div class="before-logos">
+                        <?php dynamic_sidebar('before_footer_logos_area') ?>
+                    </div>
+                    <div class="logos">
+                        <?php dynamic_sidebar('footer_logos_area') ?>
+                    </div>
+            </div>
+        </div>
+    </div>
 </footer>
 <?php wp_footer() ?>
 
--- themes/plenamata/functions.php
+++ themes/plenamata/functions.php
@@ -2,6 +2,7 @@
 namespace jaci;
 
 require __DIR__ . '/library/supports.php';
+require __DIR__ . '/library/sidebars.php';
 require __DIR__ . '/library/menus.php';
 require __DIR__ . '/library/assets.php';
 require __DIR__ . '/library/template-tags/index.php';
