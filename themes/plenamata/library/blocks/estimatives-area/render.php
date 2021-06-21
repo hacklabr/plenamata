8628142d505e5ebd18fd3453b9351cf003f3ddfb
Isaque Melo <isaquegbmelo2@gmail.com>
2021-06-21 09:04:45 -0300

    Cherry pick some files changes

--- themes/plenamata/library/blocks/estimatives-area/render.php
+++ themes/plenamata/library/blocks/estimatives-area/render.php
@@ -1,14 +1,10 @@
 <?php 
 
 function estimatives_area_render_callback($block_attributes, $content) {
-    // die("AAAAAAAAAAAAAAAAAAAAAAAAAA");
-    // echo "aaaaaaaa";
-    // ob_start();
-    // set_query_var( 'block_params', []);
-	// get_template_part( 'library/blocks/estimatives-area/', 'render');
-	// $output = ob_get_clean();
+    ob_start();
+    set_query_var( 'block_params', ['attributes' => $block_attributes]);
+	get_template_part( 'library/blocks/estimatives-area/', 'render');
+	$output = ob_get_clean();
 
-    // die();
-
-    return '<div class="asdasd">Helo from back-end</div>' ;
+    return $output ;
 }
