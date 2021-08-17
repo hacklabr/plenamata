3fca277013365d6a55acd3595578a2d7e2d3b23a
Leonardo <leopiccionia@gmail.com>
2021-08-17 17:34:14 -0300

    Add basic HTML

--- plugins/plenamata-plugin/templates/archive-verbete.php
+++ plugins/plenamata-plugin/templates/archive-verbete.php
@@ -1,56 +1,68 @@
 <?php
     $sections = get_terms( [ 'taxonomy' => 'secao', 'hide_empty' => false ] );
 ?>
-<h1>Glossário</h1>
-<div>
-    <nav class="sections">
-        <h2>
-            <?php echo __( 'Seções', 'plenamata' ) ?>
-        </h2>
-        <ul>
-            <?php foreach ( $sections as $section ): ?>
-                <li>
-                    <a href="#<?php echo $section->slug ?>">
-                        <?php echo $section->name ?>
-                    </a>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-        </nav>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo __( 'Glossário', 'plenamata' ) ?></title>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class( 'glossary' ) ?>>
+    <h1>Glossário</h1>
+    <div>
+        <nav class="sections">
+            <h2>
+                <?php echo __( 'Seções', 'plenamata' ) ?>
+            </h2>
+            <ul>
+                <?php foreach ( $sections as $section ): ?>
+                    <li>
+                        <a href="#<?php echo $section->slug ?>">
+                            <?php echo $section->name ?>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+            </nav>
 
-    <main>
-    <?php foreach ( $sections as $section ): ?>
-    <?php
-        $entries = get_posts( [
-            'post_type' => 'verbete',
-            'meta_query' => [
-                [ 'key' => 'section', 'value' => $section->term_id ],
-            ],
-        ] );
-    ?>
+        <main>
+        <?php foreach ( $sections as $section ): ?>
+        <?php
+            $entries = get_posts( [
+                'post_type' => 'verbete',
+                'meta_query' => [
+                    [ 'key' => 'section', 'value' => $section->term_id ],
+                ],
+            ] );
+        ?>
         <?php if ( !empty( $entries ) ): ?>
             <h2 id="<?php echo $section->slug ?>">
                 <?php echo __( $section->name, 'plenamata' ) ?>
             </h2>
 
             <div class="entries">
-                <?php foreach ( $entries as $entry ): ?>
-                    <details>
-                        <summary>
-                            <?php echo $entry->post_title ?>
-                        </summary>
-                        <div>
-                            <?php echo $entry->post_excerpt ?>
-                            <p>
-                                <a href="<? echo get_permalink( $entry->ID ) ?>">
-                                    <?php echo __( 'Acessar verbete completo', 'plenamata' ) ?>
-                                </a>
-                            </p>
-                        </div>
-                    </details>
-                <?php endforeach; ?>
+            <?php foreach ( $entries as $entry ): ?>
+                <details>
+                    <summary>
+                        <?php echo $entry->post_title ?>
+                    </summary>
+                    <div>
+                        <?php echo $entry->post_excerpt ?>
+                        <p>
+                            <a href="<? echo get_permalink( $entry->ID ) ?>">
+                                <?php echo __( 'Acessar verbete completo', 'plenamata' ) ?>
+                            </a>
+                        </p>
+                    </div>
+                </details>
+            <?php endforeach; ?>
             </div>
         <?php endif; ?>
-    <?php endforeach; ?>
-    </main>
-</div>
+        <?php endforeach; ?>
+        </main>
+    </div>
+</body>
+</html>
