daa47e132f9d4e52b5f6a8d209e0930c10a71729
Matheus Gimenez <contato@matheusgimenez.com.br>
2021-08-27 19:39:02 -0300

    fix header blank space

--- plugins/plenamata-plugin/templates/template-dashboard.php
+++ plugins/plenamata-plugin/templates/template-dashboard.php
@@ -7,18 +7,8 @@
  */
     $maps = get_posts(['post_type' => 'map', 'post_status' => 'publish']);
     $map_id = empty($maps) ? 0 : $maps[0]->ID;
+    get_header('single');
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php wp_head(); ?>
-</head>
-<body <?php body_class() ?>>
-    <div class="vue-dashboard-app"></div>
-    <div class="jeomap map_id_<?= $map_id ?>"></div>
-    <?php wp_footer(); ?>
-</body>
-</html>
+<div class="vue-dashboard-app"></div>
+<div class="jeomap map_id_<?= $map_id ?>"></div>
+<?php get_footer(); ?>
