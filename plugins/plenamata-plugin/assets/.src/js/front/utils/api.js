d605cf6a38f482468f4b82ad0eb14abfd7af8962
Leonardo <leopiccionia@gmail.com>
2021-08-30 15:05:40 -0300

    Fix API cache

--- plugins/plenamata-plugin/assets/.src/js/front/utils/api.js
+++ plugins/plenamata-plugin/assets/.src/js/front/utils/api.js
@@ -1,23 +1,23 @@
-const cache = {}
+const cache = new Map()
 
 async function request (method, urlFragment, cacheable = true) {
     const url = urlFragment.startsWith('http') ? urlFragment : `https://plenamata.solved.eco.br/api/${urlFragment}`
 
     const cacheKey = `${method} ${url}`
-    if (cacheable && cache[cacheKey]) {
-        Promise.resolve(cache[cacheKey])
+    if (cacheable && cache.has(cacheKey)) {
+        return Promise.resolve(cache.get(cacheKey))
     }
 
     try {
         const req = await window.fetch(url, { method })
         const data = await req.json()
         if (cacheable) {
-            cache[cacheKey] = data
+            cache.set(cacheKey, data)
         }
         return data
     } catch (err) {
         console.error(err)
-        Promise.reject(undefined)
+        return Promise.reject(undefined)
     }
 }
 
