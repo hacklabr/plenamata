869b6590602a79e29089e1b2fcece972d473377f
Leonardo <leopiccionia@gmail.com>
2021-08-31 10:53:50 -0300

    Increase charts height

--- plugins/plenamata-plugin/assets/.src/js/front/dashboard/components/YearlyDeforestationEvolutionDeter.vue
+++ plugins/plenamata-plugin/assets/.src/js/front/dashboard/components/YearlyDeforestationEvolutionDeter.vue
@@ -10,7 +10,7 @@
             </select>
         </template>
         <template #chart>
-            <BarChart :chartData="chartData" :height="200"/>
+            <BarChart :chartData="chartData" :height="300"/>
         </template>
         <template #footer>
             {{ __('Fonte: INPE/DETER', 'plenamata') }} • {{ __('Última atualização: 28.06.2021 com dados detectados até 18.06.2021.', 'plenamata') }}
