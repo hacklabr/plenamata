62598daa355f5d26acfffacc971d4dcf575ec6cf
Leonardo <leopiccionia@gmail.com>
2021-09-13 12:09:04 -0300

    Configurable i18n

--- plugins/plenamata-plugin/assets/.src/js/front/dashboard/plugins/i18n.js
+++ plugins/plenamata-plugin/assets/.src/js/front/dashboard/plugins/i18n.js
@@ -1,13 +1,14 @@
 import { sprintf } from '@wordpress/i18n'
 
-const i18n = window.PlenamataDashboard.i18n
+let i18n = null
 
 export const __ = (text, domain) => i18n?.__?.[text] ?? text
 export const _x = (text, context, domain) => i18n?._x?.[context]?.[text] ?? text
 export { sprintf }
 
 export default {
-    install (Vue) {
+    install (Vue, { locale }) {
+        i18n = locale
         Vue.prototype.__ = __
         Vue.prototype._x = _x
         Vue.prototype.sprintf = sprintf
