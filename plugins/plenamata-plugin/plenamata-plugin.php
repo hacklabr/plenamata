62f536e10734f1e98e2e3f69f285f61c415cce0b
Matheus Gimenez <matheus.gimenez@hacklab.com.br>
2021-09-27 17:59:01 -0300

    fix #33 after merge

--- plugins/plenamata-plugin/plenamata-plugin.php
+++ plugins/plenamata-plugin/plenamata-plugin.php
@@ -4,7 +4,7 @@
  *
  * Plugin Name:         Plenamata Plugin
  * Description:         The plugin adds information about the games to the site posts.
- * Version:             0.4.3
+ * Version:             0.4.4
  * Requires at least:   4.9
  * Requires PHP:        5.6
  * Author:              hacklab/
