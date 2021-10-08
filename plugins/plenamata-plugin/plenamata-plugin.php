5fff67cc419c9cc2165eda0925fdc97cf7b02a8f
Leonardo <leopiccionia@gmail.com>
2021-10-08 15:12:54 -0300

    Release v0.5.0

--- plugins/plenamata-plugin/plenamata-plugin.php
+++ plugins/plenamata-plugin/plenamata-plugin.php
@@ -4,7 +4,7 @@
  *
  * Plugin Name:         Plenamata Plugin
  * Description:         The plugin adds information about the games to the site posts.
- * Version:             0.4.5
+ * Version:             0.5.0
  * Requires at least:   4.9
  * Requires PHP:        5.6
  * Author:              hacklab/
