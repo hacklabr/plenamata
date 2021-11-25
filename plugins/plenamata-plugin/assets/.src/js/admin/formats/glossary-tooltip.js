55c53d85638d01072b6bdedc7ce47aaf05e0f691
Leonardo <leopiccionia@gmail.com>
2021-11-25 12:25:36 -0300

    Verbete selection

--- plugins/plenamata-plugin/assets/.src/js/admin/formats/glossary-tooltip.js
+++ plugins/plenamata-plugin/assets/.src/js/admin/formats/glossary-tooltip.js
@@ -1,23 +1,77 @@
 import { RichTextToolbarButton } from '@wordpress/block-editor'
+import { Modal, SelectControl } from '@wordpress/components'
+import { useSelect } from '@wordpress/data'
+import { useState } from '@wordpress/element'
 import { __ } from '@wordpress/i18n'
-import { registerFormatType, toggleFormat } from '@wordpress/rich-text'
+import { applyFormat, getActiveFormat, registerFormatType, removeFormat } from '@wordpress/rich-text'
 
-registerFormatType('plenamata/glossary-tooltip', {
+const FORMAT_NAME = 'plenamata/glossary-tooltip'
+
+function fetchVerbetes (select) {
+    return select('core').getEntityRecords('postType', 'verbete', {
+        order: 'asc',
+        orderby: 'title',
+        per_page: -1,
+    })
+}
+
+function verbeteOptions (glossary) {
+    if (!glossary) {
+        return []
+    }
+    return glossary.map((verbete) => {
+        return { label: verbete.title.raw, value: verbete.id }
+    })
+}
+
+registerFormatType(FORMAT_NAME, {
     title: __('Glossary tooltip', 'plenamata'),
     tagName: 'span',
     className: 'glossary-tooltip',
+    attributes: {
+        verbeteId: 'data-verbete-id',
+    },
 
     edit ({ isActive, onChange, value }) {
-        return (
+        const [addingTooltip, setAddingTooltip] = useState(false)
+        const [currentValue, setValue] = useState(value)
+
+        const glossary = useSelect(fetchVerbetes, [])
+
+        return <>
             <RichTextToolbarButton
                 icon="testimonial"
                 isActive={ isActive }
-                onClick={ () => {
-                    console.log('toggling...', value)
-                    onChange(toggleFormat(value, { type: 'plenamata/glossary-tooltip' }))
-                } }
                 title={ __('Glossary tooltip', 'plenamata') }
+                onClick={ () => {
+                    const activeFormat = getActiveFormat(value, FORMAT_NAME)
+                    if (activeFormat) {
+                        onChange(removeFormat(value, FORMAT_NAME))
+                    } else {
+                        setAddingTooltip(true)
+                        setValue(value)
+                    }
+                } }
             />
-        );
+            { (addingTooltip) ? (
+                <Modal
+                    title={ __('Glossary tooltip', 'plenamata') }
+                    onRequestClose={ () => {
+                        setAddingTooltip(false)
+                    } }
+                >
+                    <div className="select-glossary-tooltip">
+                        <SelectControl
+                            label={ __('Select verbete:', 'plenamata') }
+                            options={ verbeteOptions(glossary) }
+                            onChange={ (verbeteId) => {
+                                onChange(applyFormat(currentValue, { type: FORMAT_NAME, attributes: { verbeteId } }))
+                                setAddingTooltip(false)
+                            } }
+                        />
+                    </div>
+                </Modal>
+            ) : null }
+        </>;
     },
 })
