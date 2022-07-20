a3317df5671c5f3eca65202a6dd3f6e32f47dd8c
Leonardo <leopiccionia@gmail.com>
2022-07-20 16:16:34 -0300

    Fix scroll guard on monthly chart

--- plugins/plenamata-plugin/assets/.src/js/front/dashboard/components/MonthlyDeforestationEvolution.vue
+++ plugins/plenamata-plugin/assets/.src/js/front/dashboard/components/MonthlyDeforestationEvolution.vue
@@ -164,10 +164,6 @@
                 }))
             },
             months () {
-                if (this.data.length === 0) {
-                    return []
-                }
-
                 const months = this.source === 'deter'
                     ? [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
                     : [8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7]
