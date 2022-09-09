3414ab262682f083eed81921bd54108915eca8ed
Leonardo <leopiccionia@gmail.com>
2022-09-09 09:31:11 -0300

    Use previous week tree felling average if last week data is not available

--- plugins/plenamata-plugin/assets/.src/js/front/utils/estimates.js
+++ plugins/plenamata-plugin/assets/.src/js/front/utils/estimates.js
@@ -10,24 +10,26 @@ export async function getEstimateDeforestation (filters, { DateTime, Interval })
     }
 
     const previousFriday = lastFriday.minus({ weeks: 1 })
-    const penultimateFriday = previousFriday.minus({ days: 6 })
+    const penultimateSaturday = previousFriday.minus({ days: 6 })
 
-    const [thisYear, lastWeek] = await Promise.all([
+    const [thisYear, lastWeek, previousWeek] = await Promise.all([
         fetchDeterData({ ...filters, data1: now.startOf('year').toISODate(), data2: previousFriday.toISODate() }),
-        fetchDeterData({ ...filters, data1: penultimateFriday.toISODate(), data2: previousFriday.toISODate() }),
+        fetchDeterData({ ...filters, data1: penultimateSaturday.toISODate(), data2: previousFriday.toISODate() }),
+        fetchDeterData({ ...filters, data1: penultimateSaturday.minus({ weeks: 1 }).toISODate(), data2: penultimateSaturday.toISODate() }),
     ])
 
     const treesThisYear = getTrees(thisYear[0])
     const treesLastWeek = getTrees(lastWeek[0])
-    const treesPerSecondLastWeek = treesLastWeek / 604_800
+    const treesPreviousWeek = getTrees(previousWeek[0])
 
+    const treesPerSecond = (treesLastWeek || treesPreviousWeek) / 604_800
     const startDate = (lastFriday.year === now.year) ? lastFriday : now.startOf('year')
     const elapsedTime = Interval.fromDateTimes(startDate, now)
 
-    let treeCount = (treesThisYear - treesLastWeek) + (elapsedTime.count('seconds') * treesPerSecondLastWeek)
+    const treeCount = (treesThisYear - treesLastWeek) + (elapsedTime.count('seconds') * treesPerSecond)
 
     return {
-        treesPerSecond: treesPerSecondLastWeek,
+        treesPerSecond,
         trees: treeCount,
         year: now.year,
     }
