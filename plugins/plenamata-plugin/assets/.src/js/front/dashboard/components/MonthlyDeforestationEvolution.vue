454dfced7d279d6f4f0a5645ef3d91d07ded1db8
Leonardo <leopiccionia@gmail.com>
2022-09-20 17:00:50 -0300

    Revert some changes

--- plugins/plenamata-plugin/assets/.src/js/front/dashboard/components/MonthlyDeforestationEvolution.vue
+++ plugins/plenamata-plugin/assets/.src/js/front/dashboard/components/MonthlyDeforestationEvolution.vue
@@ -52,9 +52,9 @@
     export default {
         name: 'MonthlyDeforestationEvolution',
         components: {
+            Bar,
             DashboardPanel,
             Dropdown,
-            Bar,
             ScrollGuard,
         },
         mixins: [
