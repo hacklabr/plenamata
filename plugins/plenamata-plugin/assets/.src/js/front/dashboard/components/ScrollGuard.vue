987d521431606ec8984a074d7520f87b53359a2a
Leonardo <leopiccionia@gmail.com>
2023-07-19 15:57:53 -0300

    Update scroll guard text

--- plugins/plenamata-plugin/assets/.src/js/front/dashboard/components/ScrollGuard.vue
+++ plugins/plenamata-plugin/assets/.src/js/front/dashboard/components/ScrollGuard.vue
@@ -6,7 +6,7 @@
                 <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 256 256">
                     <path fill="currentColor" d="m236.5 136.5l-32 32a12.1 12.1 0 0 1-17 0a12 12 0 0 1 0-17L199 140H57l11.5 11.5a12 12 0 0 1 0 17a12.1 12.1 0 0 1-17 0l-32-32a12 12 0 0 1 0-17l32-32a12 12 0 0 1 17 17L57 116h142l-11.5-11.5a12 12 0 0 1 17-17l32 32a12 12 0 0 1 0 17Z"/>
                 </svg>
-                <span>{{ __('Drag to see more', 'plenamata') }}</span>
+                <span>{{ __('Click and drag to see more', 'plenamata') }}</span>
             </span>
         </div>
     </div>
