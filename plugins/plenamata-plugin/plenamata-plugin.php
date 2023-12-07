d2474ac87faf9602f7c3a70cb94966ce7b04343d
Matheus Gimenez <matheus.gimenez@hacklab.com.br>
2023-12-07 16:35:34 -0300

    atualiza versao do tema

--- plugins/plenamata-plugin/plenamata-plugin.php
+++ plugins/plenamata-plugin/plenamata-plugin.php
@@ -4,7 +4,7 @@
  *
  * Plugin Name:         Plenamata Plugin
  * Description:         The plugin powers deforestation dashboards and more.
- * Version:             0.20
+ * Version:             0.21
  * Requires at least:   5.0
  * Requires PHP:        7.4
  * Author:              hacklab/
--- plugins/plenamata-plugin/src/Plugin.php
+++ plugins/plenamata-plugin/src/Plugin.php
@@ -38,7 +38,7 @@ class Plugin {
 	 *
 	 * @since 0.1.0
 	 */
-	const VERSION = '0.20';
+	const VERSION = '0.21';
 
 	/**
 	 * Plugin constructor.
